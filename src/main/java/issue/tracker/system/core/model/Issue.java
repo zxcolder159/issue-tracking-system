1f3056b820311720cdecb5c7673ce2900166cdbf
colder159 <qwezitrakspsiblade@gmail.com>
2026-09-17 00:31:42 +0700

    feat: rename Task to Issue and update entity structure for issue management

---
+++ src/main/java/issue/tracker/system/core/model/Issue.java
@@ -0,0 +1,76 @@
+package issue.tracker.system.core.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.SQLRestriction;
+
+import java.time.Instant;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "issues")
+@SQLDelete(sql = "UPDATE issues SET deleted_at = CURRENT_TIMESTAMP WHERE id=?")
+@SQLRestriction("deleted_at IS NULL")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Issue {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "issue_seq_gen")
+    @SequenceGenerator(name = "issue_seq_gen", sequenceName = "issue_seq", allocationSize = 50)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String type;
+
+    @Column(columnDefinition = "TEXT")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "assignee_id")
+    private User assignee;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "issue_status_id", nullable = false)
+    private IssueStatus status;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Priority priority;
+
+
+
+    @Column(nullable = false)
+    private Instant startDate;
+
+    @Column(nullable = false)
+    private Instant dueDate;
+
+    private Long sprintId;
+
+    private Instant deletedAt;
+
+    @Column(updatable = false)
+    private Instant createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = Instant.now();
+    }
+}

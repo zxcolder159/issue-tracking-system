6f8250a80dee7f9a835abd9b62dae2540cf18c95
colder159 <qwezitrakspsiblade@gmail.com>
2026-09-17 00:32:49 +0700

    feat: add IssueStatus entity to manage issue statuses within projects

---
+++ src/main/java/issue/tracker/system/core/model/IssueStatus.java
@@ -0,0 +1,31 @@
+package issue.tracker.system.core.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "issue_statuses")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class IssueStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "issue_status_seq_gen")
+    @SequenceGenerator(name = "issue_status_seq_gen", sequenceName = "issue_status_seq", allocationSize = 50)
+    @EqualsAndHashCode.Include
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer orderIndex;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id", nullable = false)
+    private Project project;
+}

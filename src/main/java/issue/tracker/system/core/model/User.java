b71d17f41ee4c863c14549ce8f7e0270cec12aca
colder159 <qwezitrakspsiblade@gmail.com>
2026-09-17 00:32:17 +0700

    feat: enhance User entity with unique email and username constraints, update ID generation strategy

--- src/main/java/issue/tracker/system/core/model/User.java
+++ src/main/java/issue/tracker/system/core/model/User.java
@@ -1,22 +1,26 @@
 package issue.tracker.system.core.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
-@Data
+@Setter
+@Getter
 @Entity
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "user_seq_gen")
+    @SequenceGenerator(name = "user_seq_gen", sequenceName = "user_seq", allocationSize = 50)
+    @EqualsAndHashCode.Include
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String username;
 
+    @Column(unique = true, nullable = false)
     private String email;
     private String password;
 
